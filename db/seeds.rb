0f04a42ba34f9669e37117cf9d3b6e0c8bf4c7a4
renegomezjr <renegomezjr@gmail.com>
2015-11-09 14:12:23 -0500

    Assignment 30 seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,15 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by!(
+  title: "Hello this time",
+  body: "This is the time of our lives. Don't stop till you get enough.
+  This is the big one.  I really like to cha-cha.")
+
+Comment.find_or_create_by!(
+  body: "This is a unique comment.  I like the unique post.
+  It helps me do my work.  Give me hope.")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
