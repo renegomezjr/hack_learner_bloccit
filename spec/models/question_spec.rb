50c1f047895f343e5787842da794dedbf164197d
renegomezjr <renegomezjr@gmail.com>
2015-11-17 22:10:23 -0500

    Assignment 33 more crud

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) {Question.create!(title: "My new question", body: "My new body", resolved: true || false)}
+
+  describe "attributes" do
+    it "should respond to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "should respond to resolved" do
+      expect(question).to respond_to(:resolved)
+    end
+  end
+end
