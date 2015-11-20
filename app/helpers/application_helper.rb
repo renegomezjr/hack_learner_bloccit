806425f1d90a6c9a11ce35281493f725d66a9dd5
renegomezjr <renegomezjr@gmail.com>
2015-11-20 14:37:42 -0500

    Checkpoint 35 validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: "form-grou has-error"
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,15 @@ include RandomData
 
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  
-  let(:post) {topic.posts.create!(title: "New Post Title", body: "New Post Body") }
+
+  let(:post) {topic.posts.create!(title: "New Post Title", body: "This is the New Post Body. There is none like it.") }
 
   it { should belong_to(:topic) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "should respond to title" do
