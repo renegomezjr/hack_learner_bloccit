f0a846f1479a7ec93d1941f4ddf799846747fe16
renegomezjr <renegomezjr@gmail.com>
2015-11-21 22:53:30 -0500

    1st round assignment 36

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { User.create!(name: "bloccit user", email: "user@bloccit.com", password: "password") }
 
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
@@ -23,6 +23,10 @@ RSpec.describe User, type: :model do
 
     it "should respond to email" do
       expect(user).to respond_to(:email)
+    end
+
+    it "should capitalize first name and last name" do
+      expect(user.name).to eq "Bloccit User"
     end
   end
 
