17cd41611e498277a502a80eeeada2e1d7db5684
renegomezjr <renegomezjr@gmail.com>
2015-11-23 12:17:16 -0500

    first commit checkpoint 39

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,9 @@ include RandomData
 RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
 
-  let(:post) { topic.post.create!(title: "New Post Title", body: "New Post Body") }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+
+  let(:post) { topic.post.create!(title: "New Post Title", body: "New Post Body. This is it.", user: user) }
 
   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
