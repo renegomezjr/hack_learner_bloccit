103392a0af0a2d55cbee611677c1f423d09b7f6c
renegomezjr <renegomezjr@gmail.com>
2015-12-08 17:45:43 -0500

    Checkpoint 48 api retrieving

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,11 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index' #this is an implied hash
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
